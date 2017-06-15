4b3195df95535a50bb907a0c6aa24abed5fc39af
Adrian Likins <alikins@redhat.com>
2017-06-15 18:05:41 -0400

    a couple of tests

--- tests/test_color_debug.py
+++ tests/test_color_debug.py
@@ -5,6 +5,7 @@
 
 import pytest
 
+import logging
 
 from color_debug import color_debug
 
@@ -19,7 +20,19 @@ def response():
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_find_format_attrs():
+    format_string = 'foo %(process)d blip'
+    res = color_debug.find_format_attrs(format_string)
+    assert ('%(process)d', 'process') in res
+
+
+def test_init(response):
+    """Just init the class"""
+    formatter = color_debug.ColorFormatter()
+    assert isinstance(formatter, color_debug.ColorFormatter)
+
+
+def test_null_handler(response):
+    nh = logging.NullHandler()
+    formatter = color_debug.ColorFormatter()
+    nh.setFormatter(formatter)
