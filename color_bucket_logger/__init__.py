76f81843e4babd9cfa28418e35faa42eb22c4e91
Adrian Likins <alikins@redhat.com>
2019-05-05 14:51:05 -0400

    mv other_module to examples/

--- color_bucket_logger/__init__.py
+++ color_bucket_logger/__init__.py
@@ -7,6 +7,5 @@ __email__ = 'adrian@likins.com'
 __version__ = '0.1.0'
 
 from .formatter import ColorFormatter, add_default_record_attrs
-from . import other_module
 
-__all__ = ['ColorFormatter', 'add_default_record_attrs', 'other_module']
+__all__ = ['ColorFormatter', 'add_default_record_attrs']
